6afc323d75631407aaf2116342cfd6101b52c5ea
sevinc995 <sevincseferova25@gmail.com>
2025-05-31 09:34:15 +0000

    link

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=1389e219f06c8f6506e7f45269403323`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=1389e219f06c8f6506e7f45269403323&units=metric`
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
@@ -19,7 +19,7 @@ $(document).ready(function () {
 
     $("#btn").click(function (e) {
         e.preventDefault()
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=1389e219f06c8f6506e7f45269403323`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=1389e219f06c8f6506e7f45269403323&units=metric`
         async function myweather() {
             let responce = await fetch(url)
             let data = await responce.json()
